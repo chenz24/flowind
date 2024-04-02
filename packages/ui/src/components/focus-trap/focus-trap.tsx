b6274f4a3fc073b2d40969aeaa218b655817c0a0
chenz24 <chenz8606@gmail.com>
2024-04-02 08:42:02 +0800

    feat: add focus-trap component

---
+++ packages/ui/src/components/focus-trap/focus-trap.tsx
@@ -0,0 +1,32 @@
+import React, { cloneElement } from 'react';
+
+import { useFocusTrap, useMergedRef } from '@flowind/hooks';
+import { isElement } from '@/utils/is-element/is-element';
+
+export interface FocusTrapProps {
+  /** Element at which focus should be trapped, should support ref prop */
+  children: any;
+
+  /** Determines whether focus should be trapped within child element */
+  active?: boolean;
+
+  /** Prop that should be used to access component ref */
+  refProp?: string;
+}
+
+export function FocusTrap({
+  children,
+  active = true,
+  refProp = 'ref',
+}: FocusTrapProps): React.ReactElement {
+  const focusTrapRef = useFocusTrap(active);
+  const ref = useMergedRef(focusTrapRef, children?.ref);
+
+  if (!isElement(children)) {
+    return children;
+  }
+
+  return cloneElement(children, { [refProp]: ref });
+}
+
+FocusTrap.displayName = 'FocusTrap';
