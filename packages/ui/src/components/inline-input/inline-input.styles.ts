20ba237e4c5bedf7d65d146e4d13854ff1618340
chenz24 <chenz8606@gmail.com>
2024-05-07 13:46:40 +0800

    refactor: refine inline-input component

--- packages/ui/src/components/inline-input/inline-input.styles.ts
+++ packages/ui/src/components/inline-input/inline-input.styles.ts
@@ -1,19 +1,11 @@
-import { createStyles, rem } from '@/styles';
-
-const sizes = {
-  xs: rem(16),
-  sm: rem(20),
-  md: rem(24),
-  lg: rem(30),
-  xl: rem(36),
-};
+import { createStyles } from '@/styles';
 
 const fontSizes = {
-  xs: 'text-xs',
-  sm: 'text-sm',
-  md: 'text-base',
-  lg: 'text-lg',
-  xl: 'text-xl',
+  xs: 'text-xs leading-[14px]',
+  sm: 'text-sm leading-4',
+  md: 'text-base leading-5',
+  lg: 'text-lg leading-6',
+  xl: 'text-xl leading-7',
 };
 
 export interface InlineInputStylesParams {
@@ -34,42 +26,4 @@ export default createStyles((theme, { labelPosition }: InlineInputStylesParams, 
       label: `${padding} text-slate-900 dark:text-slate-300 data-[disabled=true]:opacity-60`,
     },
   };
-  // body: {
-  //   display: 'flex',
-  //
-  //   '&:has(input:disabled) label': {
-  //     color: theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.gray[5],
-  //   },
-  // },
-  //
-  // labelWrapper: {
-  //   ...theme.fn.fontStyles(),
-  //   display: 'inline-flex',
-  //   flexDirection: 'column',
-  //   WebkitTapHighlightColor: 'transparent',
-  //   fontSize: size in sizes ? getSize({ size, sizes: theme.fontSizes }) : undefined,
-  //   lineHeight: size in sizes ? getSize({ size, sizes }) : undefined,
-  //   color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
-  //   cursor: theme.cursorType,
-  //   order: labelPosition === 'left' ? 1 : 2,
-  // },
-  //
-  // description: {
-  //   marginTop: `calc(${theme.spacing.xs} / 2)`,
-  //   [labelPosition === 'left' ? 'paddingRight' : 'paddingLeft']: theme.spacing.sm,
-  // },
-  //
-  // error: {
-  //   marginTop: `calc(${theme.spacing.xs} / 2)`,
-  //   [labelPosition === 'left' ? 'paddingRight' : 'paddingLeft']: theme.spacing.sm,
-  // },
-  //
-  // label: {
-  //   cursor: theme.cursorType,
-  //   [labelPosition === 'left' ? 'paddingRight' : 'paddingLeft']: theme.spacing.sm,
-  //
-  //   '&:disabled, &[data-disabled]': {
-  //     color: theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.gray[5],
-  //   },
-  // },
 });
