314d95c2d02c3ff613a4747e926d81f57d0da8eb
chenz24 <chenz8606@gmail.com>
2024-05-15 16:00:17 +0800

    refactor: refine divider component

--- packages/ui/src/components/divider/index.ts
+++ packages/ui/src/components/divider/index.ts
@@ -1,1 +1,3 @@
-export * from './divider';
+export { Divider } from './divider';
+export type { DividerProps, DividerStylesNames } from './divider';
+export type { DividerStylesParams } from './divider.styles';
