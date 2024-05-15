26d319cb8f34d2d700eb3b82942e7034ca4680c3
chenz24 <chenz8606@gmail.com>
2024-05-15 15:35:05 +0800

    refactor: add use client to color-picker component

--- packages/ui/src/components/color-picker/saturation/saturation.tsx
+++ packages/ui/src/components/color-picker/saturation/saturation.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useEffect, useRef, useState } from 'react';
 
 import { clampUseMovePosition, useMove, UseMovePosition } from '@flowind/hooks';
--- packages/ui/src/components/color-picker/thumb/thumb.tsx
+++ packages/ui/src/components/color-picker/thumb/thumb.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 
 import { DefaultProps, FlowindSize, Selectors } from '@/styles';
