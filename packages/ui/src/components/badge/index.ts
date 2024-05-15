db36afee9383f8f5dbff49e86717c1c7c67868c4
chenz24 <chenz8606@gmail.com>
2024-05-15 11:27:51 +0800

    refactor: refine badge component

--- packages/ui/src/components/badge/index.ts
+++ packages/ui/src/components/badge/index.ts
@@ -1,1 +1,3 @@
-export * from "./badge"
+export { Badge } from './badge';
+export type { BadgeProps, BadgeStylesNames } from './badge';
+export type { BadgeStylesParams, BadgeVariant } from './badge.styles';
