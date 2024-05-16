6f7d249414d1103142068d8e5a29e87a7f5744f5
chenz24 <chenz8606@gmail.com>
2024-05-16 13:20:20 +0800

    refactor: refine scroll-area component

--- packages/ui/src/components/scroll-area/index.ts
+++ packages/ui/src/components/scroll-area/index.ts
@@ -1,1 +1,7 @@
-export * from './scroll-area';
+export { ScrollArea } from './scroll-area';
+export type {
+  ScrollAreaStylesNames,
+  ScrollAreaProps,
+  ScrollAreaAutosizeProps,
+} from './scroll-area';
+export type { ScrollAreaStylesParams } from './scroll-area.styles';
