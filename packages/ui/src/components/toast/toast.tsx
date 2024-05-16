8eaf2762559c0d8f064c58c2c0d1375131febaa2
chenz24 <chenz8606@gmail.com>
2024-05-16 16:04:08 +0800

    refactor: refine toaster component

--- packages/ui/src/components/toast/toast.tsx
+++ packages/ui/src/components/toast/toast.tsx
@@ -1,7 +1,4 @@
-'use client';
-
 import * as React from 'react';
-import * as Primitives from '@radix-ui/react-toast';
 
 import {
   CheckCircleSolid,
@@ -10,166 +7,142 @@ import {
   Spinner,
   XCircleSolid,
 } from '@flowind/icons';
+import type { ToastAction, ToastVariant } from '@/types';
 import { clx } from '@/utils/clx';
 
-const ToastProvider = Primitives.Provider;
-ToastProvider.displayName = 'ToastProvider';
-
-const ToastViewport = React.forwardRef<
-  React.ElementRef<typeof Primitives.Viewport>,
-  React.ComponentPropsWithoutRef<typeof Primitives.Viewport>
->(({ className, ...props }, ref) => (
-  <Primitives.Viewport
-    ref={ref}
-    className={clx('fixed right-0 top-0 z-[9999] w-full p-6 md:max-w-[484px]', className)}
-    {...props}
-  />
-));
-ToastViewport.displayName = 'Toast.Viewport';
-
-interface ActionProps {
-  label: string;
-  altText: string;
-  onClick: () => void | Promise<void>;
-}
-
-interface ToastProps extends React.ComponentPropsWithoutRef<typeof Primitives.Root> {
-  variant?: 'info' | 'success' | 'warning' | 'error' | 'loading';
-  title?: string;
+interface ToastComponentProps {
+  id: string | number;
+  variant?: ToastVariant;
+  title: string;
   description?: string;
-  action?: ActionProps;
-  disableDismiss?: boolean;
+  action?: ToastAction;
+  onDismiss?: (id?: string | number) => void;
+  dismissLabel?: string;
 }
 
 /**
- * This component is based on the [Radix UI Toast](https://www.radix-ui.com/primitives/docs/components/toast) primitives.
- *
- * @excludeExternal
+ * This component is based on the [Sonner](https://sonner.emilkowal.ski/toast) toast library.
  */
-const Toast = React.forwardRef<React.ElementRef<typeof Primitives.Root>, ToastProps>(
-  (
-    {
-      className,
-      /**
-       * The toast's style.
-       */
-      variant,
-      /**
-       * The toast's title.
-       */
-      title,
-      /**
-       * The toast's content.
-       */
-      description,
-      /**
-       * The actions to show in the toast as buttons.
-       */
-      action,
-      /**
-       * Whether to hide the Close button.
-       */
-      disableDismiss = false,
-      ...props
-    }: ToastProps,
-    ref,
-  ) => {
-    let Icon;
+export const Toast = ({
+  /**
+   * Optional ID of the toast.
+   */
+  id,
+  /**
+   * @ignore
+   *
+   * @privateRemarks
+   * As the Toast component is created using
+   * the `toast` utility functions, the variant is inferred
+   * from the utility function.
+   */
+  variant = 'info',
+  /**
+   * @ignore
+   *
+   * @privateRemarks
+   * The `toast` utility functions accept this as a parameter.
+   */
+  title,
+  /**
+   * The toast's text.
+   */
+  description,
+  /**
+   * The toast's action buttons.
+   */
+  action,
+  /**
+   * @ignore
+   *
+   * @privateRemarks
+   * The `toast` utility functions don't allow
+   * passing this prop.
+   */
+  onDismiss,
+  /**
+   * The label of the dismiss button, if available.
+   */
+  dismissLabel = 'Close',
+}: ToastComponentProps) => {
+  const hasActionables = !!action || onDismiss;
+  let icon;
 
-    switch (variant) {
-      case 'success':
-        Icon = <CheckCircleSolid className="text-tag-green-icon" />;
-        break;
-      case 'warning':
-        Icon = <ExclamationCircleSolid className="text-tag-orange-icon" />;
-        break;
-      case 'error':
-        Icon = <XCircleSolid className="text-tag-red-icon" />;
-        break;
-      case 'loading':
-        Icon = <Spinner className="text-tag-blue-icon animate-spin" />;
-        break;
-      default:
-        Icon = <InformationCircleSolid className="text-fg-base" />;
-        break;
-    }
+  switch (variant) {
+    case 'success':
+      icon = <CheckCircleSolid size="20" className="text-tag-green-icon" />;
+      break;
+    case 'warning':
+      icon = <ExclamationCircleSolid size="20" className="text-tag-orange-icon" />;
+      break;
+    case 'error':
+      icon = <XCircleSolid size="20" className="text-tag-red-icon" />;
+      break;
+    case 'loading':
+      icon = <Spinner size="20" className="text-tag-blue-icon animate-spin" />;
+      break;
+    case 'info':
+      icon = <InformationCircleSolid size="20" className="text-fg-base" />;
+      break;
+    default:
+      break;
+  }
 
-    if (action && !action.altText) {
-      if (process.env.NODE_ENV === 'development') {
-        console.warn(
-          'Omitting `altText` from the action is not recommended. Please provide a description for screen readers.',
-        );
-      }
-    }
-
-    return (
-      <Primitives.Root
-        ref={ref}
-        className={clx(
-          'bg-bg-base border-border-base flex h-fit min-h-[74px] w-full overflow-hidden rounded-md border shadow-[0_4px_12px_rgba(0,0,0,0.05)] md:max-w-[440px]',
-          'data-[state=open]:animate-in data-[state=closed]:animate-out data-[swipe=end]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:slide-out-to-right-full data-[state=open]:slide-in-from-top-full data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=move]:transition-none',
-          className,
-        )}
-        {...props}
+  return (
+    <div className="shadow-elevation-flyout bg-bg-base flex w-fit min-w-[360px] max-w-[440px] overflow-hidden rounded-lg">
+      <div
+        className={clx('grid flex-1 items-start gap-3 py-3 pl-3', {
+          'border-r pr-3': hasActionables,
+          'pr-6': !hasActionables,
+          'grid-cols-[20px_1fr]': !!icon,
+          'grid-cols-1': !icon,
+        })}
       >
-        <div className="border-border-base flex flex-1 items-start space-x-3 border-r p-4">
-          <span aria-hidden>{Icon}</span>
-          <div>
-            {title && (
-              <Primitives.Title className="text-fg-base txt-compact-sm-plus">
-                {title}
-              </Primitives.Title>
-            )}
-            {description && (
-              <Primitives.Description className="text-fg-subtle txt-compact-md">
-                {description}
-              </Primitives.Description>
-            )}
-          </div>
-        </div>
+        {!!icon && (
+          <span className="flex size-5 items-center justify-center" aria-hidden>
+            {icon}
+          </span>
+        )}
         <div className="flex flex-col">
-          {action && (
-            <>
-              <Primitives.Action
-                altText={action.altText}
-                className={clx(
-                  'txt-compact-sm-plus text-fg-base bg-bg-base hover:bg-bg-base-hover active:bg-bg-base-pressed flex flex-1 items-center justify-center px-6 transition-colors',
-                  {
-                    'text-fg-error': variant === 'error',
-                  },
-                )}
-                onClick={(e) => {
-                  e.preventDefault();
-                  action.onClick();
-                }}
-                type="button"
-              >
-                {action.label}
-              </Primitives.Action>
-              <div className="bg-border-base h-px w-full" />
-            </>
-          )}
-          {!disableDismiss && (
-            <Primitives.Close
+          <span className="txt-compact-sm-plus text-fg-base">{title}</span>
+          <span className="txt-sm text-fg-subtle text-pretty">{description}</span>
+        </div>
+      </div>
+      {hasActionables && (
+        <div
+          className={clx('grid grid-cols-1', {
+            'grid-rows-2': !!action && onDismiss,
+            'grid-rows-1': !action || !onDismiss,
+          })}
+        >
+          {!!action && (
+            <button
+              type="button"
               className={clx(
-                'txt-compact-sm-plus text-fg-subtle bg-bg-base hover:bg-bg-base-hover active:bg-bg-base-pressed flex flex-1 items-center justify-center px-6 transition-colors',
+                'txt-compact-sm-plus text-fg-base bg-bg-base hover:bg-bg-base-hover active:bg-bg-base-pressed flex items-center justify-center px-4 transition-colors',
                 {
-                  'h-1/2': action,
-                  'h-full': !action,
+                  'border-border-base border-b': onDismiss,
+                  'text-fg-error': action.variant === 'destructive',
                 },
               )}
-              aria-label="Close"
+              onClick={action.onClick}
             >
-              Close
-            </Primitives.Close>
+              {action.label}
+            </button>
+          )}
+          {onDismiss && (
+            <button
+              type="button"
+              onClick={() => onDismiss(id)}
+              className={clx(
+                'txt-compact-sm-plus text-fg-subtle bg-bg-base hover:bg-bg-base-hover active:bg-bg-base-pressed flex items-center justify-center px-4 transition-colors',
+              )}
+            >
+              {dismissLabel}
+            </button>
           )}
         </div>
-      </Primitives.Root>
-    );
-  },
-);
-Toast.displayName = 'Toast';
-
-type ToastActionElement = ActionProps;
-
-export { Toast, ToastProvider, ToastViewport, type ToastActionElement, type ToastProps };
+      )}
+    </div>
+  );
+};
