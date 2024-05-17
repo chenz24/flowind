42d5123194f647592cb5236eee600232f97b9c2e
chenz24 <chenz8606@gmail.com>
2024-05-17 11:07:29 +0800

    feat: add NavigationMenu component

---
+++ packages/ui/src/components/navigation-menu/navigation-menu.styles.ts
@@ -0,0 +1,10 @@
+import { createStyles } from '@/styles';
+
+export default createStyles(() => ({
+  classes: {
+    root: 'relative z-10 flex max-w-max flex-1 items-center justify-center',
+  },
+  styles: {
+    root: {},
+  },
+}));
