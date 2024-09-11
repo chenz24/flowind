542138b2a4e4edc9e75f2e0439f9fdee595521f6
chenz24 <chenz8606@gmail.com>
2024-09-11 22:00:17 +0800

    feat: add FileButton component

---
+++ packages/ui/src/components/file-button/index.ts
@@ -0,0 +1,2 @@
+export { FileButton } from './file-button';
+export type { FileButtonProps } from './file-button';
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -25,6 +25,7 @@ export * from './components/date-picker';
 export * from './components/divider';
 export * from './components/drawer';
 export * from './components/dropdown-menu';
+export * from './components/file-button';
 export { FocusModal } from './components/focus-modal';
 export * from './components/focus-trap';
 export * from './components/heading';
